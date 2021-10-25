81e68125826968637c89c6a9ad8d3ffae6b9e0cb
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-10-25 14:32:54 +0530

    add put

--- src/components/UpdateProduct/UpdateProduct.js
+++ src/components/UpdateProduct/UpdateProduct.js
@@ -11,18 +11,40 @@ const UpdateProducts = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                // console.log(data);
                 setProduct(data);
             })
     }, [])
 
-    const handleName = () => {
-
+    const handleName = e => {
+        const newName = e.target.value;
+        const updatedProduct = { ...product };
+        updatedProduct.name = newName;
+        setProduct(updatedProduct);
     }
-    const handlePrice = () => {
-
+    const handlePrice = e => {
+        const newPrice = e.target.value;
+        const updatedProduct = { ...product };
+        updatedProduct.price = newPrice;
+        setProduct(updatedProduct);
     }
-    const handleQuantity = () => {
+    const handleQuantity = e => {
+        const newQuantity = e.target.value;
+        const updatedProduct = { ...product };
+        updatedProduct.quantity = newQuantity;
+        setProduct(updatedProduct);
+    }
+
+    const handleUpdate = (e, id) => {
+        e.preventDefault();
+        // const url = `http://localhost:5000/products/updateproduct/${id}`
+        // fetch(url, {
+        //     method: 'PUT',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(product)
+        // })
 
     }
     return (
@@ -35,10 +57,11 @@ const UpdateProducts = () => {
                 <p>Price:{product.price}</p>
                 <p>Quantity:{product.quantity}</p>
             </div>
-            <form onSubmit="">
+            <form onSubmit={() => { handleUpdate(product._id) }}>
                 <input type="text" onChange={handleName} value={product.name} />
                 <input type="number" onChange={handlePrice} value={product.price} />
                 <input type="number" onChange={handleQuantity} value={product.quantity} />
+                <input type="submit" value="Update" />
             </form>
         </div>
     );
